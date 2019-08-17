b518cb7170ac35a3ea685af156e59a33c042c0f9
Artem Grachov <grachov2195@gmail.com>
2019-08-18 00:08:56 +0300

    Password update

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const validator = require('validator');
 const User  = require('../models/user');
 const config = require('../configs/main');
 const errors = require('../configs/errors');
+const errorFactory = require('../utils/error-factory');
 
 exports.registration = async (req, res, next) => {
   try {
@@ -17,34 +18,29 @@ exports.registration = async (req, res, next) => {
 
     if (!validator.isEmail(email)) {
         validationErrors.push('Invalid email');
-    };
+    }
     if (!validator.isLength(password, { min: 8, max: 18 })) {
         validationErrors.push('Invalid password');
     };
     if (password !== passwordConfirmation) {
         validationErrors.push('Passwords are not equal');
-    };
+    }
     if (!firstname) {
         validationErrors.push('First name is required');
-    };
+    }
     if (!lastname) {
         validationErrors.push('Last name is required');
-    };
+    }
     
     if (validationErrors.length) {
-        const error = new Error(errors.INVALID_INPUT);
-        error.data = validationErrors;
-        error.statusCode = 422;
-        throw error;
-    };
+        throw errorFactory(422, errors.INVALID_INPUT, validationErrors);
+    }
 
     const registeredUser = await User.findOne({ email });
     
     if (registeredUser) {
-        const error = new Error(errors.EMAIL_ALREADY_REGISTERED);
-        error.statusCode = 422;
-        throw error;
-    };
+        throw errorFactory(422, errors.EMAIL_ALREADY_REGISTERED);
+    }
 
     const passwordHash = await bcrypt.hash(password, 12);
 
@@ -62,8 +58,8 @@ exports.registration = async (req, res, next) => {
         .json({ message: 'User registered successfully' });
     } catch(err) {
         next(err);
-    };
-};
+    }
+}
 
 exports.login = async (req, res, next) => {
     const email = req.body.email;
@@ -73,18 +69,14 @@ exports.login = async (req, res, next) => {
 
     try {
         if (!user) {
-            const error = new Error(errors.INCORRECT_EMAIL_OR_PASSWORD);
-            error.statusCode = 401;
-            throw error;
-        };
+            throw errorFactory(401, errors.INCORRECT_EMAIL_OR_PASSWORD);
+        }
 
         const passwordCheck = await bcrypt.compare(password, user.password);
 
         if (!passwordCheck) {
-            const error = new Error(errors.INCORRECT_EMAIL_OR_PASSWORD);
-            error.statusCode = 401;
-            throw error;
-        };
+            throw errorFactory(401, errors.INCORRECT_EMAIL_OR_PASSWORD);
+        }
 
         const token = jwt.sign(
             { userId: user._id.toString() },
@@ -97,5 +89,46 @@ exports.login = async (req, res, next) => {
         .json({ token });
     } catch (err) {
         next(err);
-    };
-};
+    }
+}
+
+exports.changePassword = async (req, res, next) => {
+    try {
+        const password = req.body.password;
+        const passwordConfirmation = req.body.passwordConfirmation;
+
+        if (!password || !passwordConfirmation) {
+            throw errorFactory(422, errors.INVALID_INPUT);
+        }
+
+        const validationErrors = [];
+
+        if (!validator.isLength(password, { min: 8, max: 18 })) {
+            validationErrors.push('Invalid password');
+        }
+
+        if (password !== passwordConfirmation) {
+            validationErrors.push('Passwords are not equal');
+        }
+
+        if (validationErrors.length) {
+            throw errorFactory(422, errors.INVALID_INPUT, validationErrors);
+        }
+
+        const passwordHash = await bcrypt.hash(password, 12);
+
+        const user = await User.findById(req.userId);
+
+        user.set({ password: passwordHash });
+
+        await user.save();
+
+        res
+            .status(200)
+            .json({
+                message: 'Password updated successfully'
+            })
+    } catch(err) {
+        next(err);
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const checkAuth = require('../middlewares/check-auth');
 const authController = require('../controllers/auth');
 
 router.post('/registration', authController.registration);
 
 router.post('/login', authController.login);
+
+router.patch('/password', checkAuth, authController.changePassword);
 
 module.exports = router;
